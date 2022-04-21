85c1befa79feb917baae1b3b27d2914775b5e9ef
Raphael Höser <raphael@hoeser.info>
2022-04-21 23:45:29 +0200

    Add sync resolver to nominatim
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- src/resolvers/nominatim.rs
+++ src/resolvers/nominatim.rs
@@ -10,6 +10,24 @@ struct OSMLocation {
     lon: String,
 }
 
+/// Resolve a location synchronously name to a coordinate
+pub fn resolve_sync(location: &str) -> Result<Coordinate, CoordinateError> {
+    let locations = reqwest::blocking::Client::new()
+        .get("https://nominatim.openstreetmap.org/search")
+        .header(reqwest::header::USER_AGENT, "tanker_price")
+        .query(&[("format", "json"), ("q", location)])
+        .send()?
+        .json::<Vec<OSMLocation>>()?;
+    if let Some(location) = locations.get(0) {
+        Ok(Coordinate {
+            lng: location.lon.parse()?,
+            lat: location.lat.parse()?,
+        })
+    } else {
+        Err(CoordinateError::Unresolveable)
+    }
+}
+
 /// Resolve a location name to a coordinate
 pub async fn resolve(location: &str) -> Result<Coordinate, CoordinateError> {
     let locations = reqwest::Client::new()
